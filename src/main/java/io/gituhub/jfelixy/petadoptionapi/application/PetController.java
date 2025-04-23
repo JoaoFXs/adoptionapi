7f87d727fa336cc458692833354ff7805f6288d6
jovibfel@gmail.com <jovibfel@gmail.com>
2025-04-23 19:33:48 -0300

    Update Controller to receive a object in save requesting

--- src/main/java/io/gituhub/jfelixy/petadoptionapi/application/PetController.java
+++ src/main/java/io/gituhub/jfelixy/petadoptionapi/application/PetController.java
@@ -4,10 +4,7 @@ package io.gituhub.jfelixy.petadoptionapi.application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,11 +14,8 @@ public class PetController {
 
     private static final Logger log = LoggerFactory.getLogger(PetController.class);
     @PostMapping
-    public ResponseEntity save(@RequestParam("name") String name,
-                               @RequestParam("age") String age,
-                               @RequestParam("tags") List<String> tags
-                               ){
-        log.info("Pet Received: name: {}, age: {}, tags: {}",name, age, String.join(",", tags));
+    public ResponseEntity save(@RequestBody Pet pet){
+        //log.info("Pet Received: name: {}, age: {}, tags: {}",name, age, String.join(",", tags));
         return ResponseEntity.ok().build();
     }
 
