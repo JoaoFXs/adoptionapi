c047cb0b3af41709bff62e4047fe5a63b589e934
jovibfel@gmail.com <jovibfel@gmail.com>
2025-04-24 16:59:45 -0300

    Remove TemperamentEnum.java

--- src/main/java/io/gituhub/jfelixy/petadoptionapi/domain/enums/TemperamentEnum.java
+++ src/main/java/io/gituhub/jfelixy/petadoptionapi/domain/enums/TemperamentEnum.java
@@ -1,19 +1,5 @@
 package io.gituhub.jfelixy.petadoptionapi.domain.enums;
 
 public enum TemperamentEnum {
-    DOCILE,
-    AGGRESSIVE,
-    FEARFUL,
-    PLAYFUL,
-    PROTECTIVE,
-    INDEPENDENT,
-    CALM,
-    ANXIOUS,
-    CURIOUS,
-    TERRITORIAL;
-    @Override
-    public String toString() {
-        String name = name().toLowerCase().replace("_", " ");
-        return Character.toUpperCase(name.charAt(0)) + name.substring(1);
-    }
+
 }
