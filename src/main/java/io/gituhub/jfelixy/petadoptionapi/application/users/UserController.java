2fadce281e0b4637ec8218fa7f1a4e2656327503
jovibfel@gmail.com <jovibfel@gmail.com>
2025-05-05 19:55:36 -0300

    Create Save Default controller

--- src/main/java/io/gituhub/jfelixy/petadoptionapi/application/users/UserController.java
+++ src/main/java/io/gituhub/jfelixy/petadoptionapi/application/users/UserController.java
@@ -2,14 +2,35 @@ package io.gituhub.jfelixy.petadoptionapi.application.users;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/users")
 @RequiredArgsConstructor
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserMapper userMapper;
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody UserDTO dto){
+        try{
+
+        }catch (DuplicatedTupleException e){
+            Map<String, String> jsonResult = Map.of("error", e.getMessage());
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(jsonResult);
+        }
+    }
 
 
 
